06f8fbcba2f44f1ed898b72b38dd59fe3427a78c
MarijnSt <marijn.stammeleer@gmail.com>
2025-11-10 18:48:06 +0100

    feat: transform tracking data to long format

--- src/transform/__init__.py
+++ src/transform/__init__.py
@@ -1,9 +1,11 @@
 from .passing_network_data import get_passing_network_data
 from .territorial_heatmap_data import get_territorial_heatmap_data
+from .long_format_tracking_data import transform_tracking_to_long_format
 from .block_by_ball_position import get_ball_zone
 
 __all__ = [
     'get_passing_network_data',
     'get_territorial_heatmap_data',
-    'get_ball_zone'
+    'transform_tracking_to_long_format'
+    'get_ball_zone',
 ]
